6be50269c61dcafc2469df4fe1600fb133f2bdc2
Indrek Juhkam <indrek@urgas.eu>
2016-03-07 20:22:17 +0200

    Reduce duplication in FreddySpec

--- test/Network/FreddySpec.hs
+++ test/Network/FreddySpec.hs
@@ -7,52 +7,42 @@ import Data.UUID (UUID, toText)
 import Data.Text (Text)
 import Control.Concurrent (threadDelay)
 import qualified Network.Freddy as Freddy
-
-newUUID :: IO UUID
-newUUID = randomIO
-
-randomQueueName :: IO Text
-randomQueueName = do
-  uuid <- newUUID
-  return $ toText uuid
-
-echoResponder (Freddy.Request body replyWith failWith) = do
-  replyWith body
-
-delayedResponder (Freddy.Request body replyWith failWith) = do
-  threadDelay $ 4 * 1000 * 1000 -- 4 seconds
-  replyWith body
+import SpecHelper (
+  newUUID,
+  randomQueueName,
+  echoResponder,
+  delayedResponder,
+  connect,
+  requestBody
+  )
 
 spec :: Spec
 spec = do
   describe "Freddy" $ do
     it "can respond to messages" $ do
-      (respondTo, deliverWithResponse) <- Freddy.connect "127.0.0.1" "/" "guest" "guest"
+      (respondTo, deliverWithResponse) <- connect
       queueName <- randomQueueName
 
       respondTo queueName echoResponder
 
-      let requestBody = "msg body"
       let response = deliverWithResponse queueName requestBody
 
       response `shouldReturn` Right requestBody
 
     it "handles timeouts" $ do
-      (respondTo, deliverWithResponse) <- Freddy.connect "127.0.0.1" "/" "guest" "guest"
+      (respondTo, deliverWithResponse) <- connect
       queueName <- randomQueueName
 
       respondTo queueName delayedResponder
 
-      let requestBody = "msg body"
       let response = deliverWithResponse queueName requestBody
 
       response `shouldReturn` Left Freddy.TimeoutError
 
     it "returns a invalid request error when queue does not exist" $ do
-      (respondTo, deliverWithResponse) <- Freddy.connect "127.0.0.1" "/" "guest" "guest"
+      (_, deliverWithResponse) <- connect
       queueName <- randomQueueName
 
-      let requestBody = "msg body"
       let response = deliverWithResponse queueName requestBody
 
       response `shouldReturn` Left Freddy.InvalidRequest
